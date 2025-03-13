7e08f6ca7085294926367a02a48af82fc494dc90
Juliano Soares da Silva <bytebytebug@gmail.com>
2025-03-13 12:21:48 -0300

    feat: add response errors

--- src/core/rent/adapter/express/handler/create-blade.ts
+++ src/core/rent/adapter/express/handler/create-blade.ts
@@ -1,6 +1,8 @@
 import { IBladeService } from "#core/rent/application/blade-service";
 import { Request, Response } from "express";
 import { CreateBladeInputParser } from "#core/rent/adapter/parser/blade";
+import { ParsingError } from "../../parser/parser";
+import { ApplicationError } from "#core/rent/application/errors/application";
 
 
 export class CreateBladeHandler {
@@ -14,16 +16,29 @@ export class CreateBladeHandler {
 
     async execute(req: Request, res: Response) {
 
-        let { name, description, price } = this.parser.parse(req.body);
+        try {
+            let { name, description, price } = this.parser.parse(req.body);
 
-        let id = await this.bladeService.createBlade({
-            name,
-            description,
-            price,
-        })
+            let id = await this.bladeService.createBlade({
+                name,
+                description,
+                price,
+            })
 
-        res.status(201).json({
-            id,
-        })
+            res.status(201).json({
+                id,
+            })
+        } catch (e) {
+            if (e instanceof ParsingError) {
+                res.status(400).send({
+                    message: "input error"
+                })
+                return;
+            }
+
+            res.status(500).send({
+                message: "Server error"
+            })
+        }
     }
 }
